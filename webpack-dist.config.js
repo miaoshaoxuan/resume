23d0ef39401cdefca68738a0a486677e8091f948
Miaoshaoxuan <47883920+MsxDD@users.noreply.github.com>
2020-08-18 16:36:56 +0800

    Update webpack-dist.config.js

--- webpack-dist.config.js
+++ webpack-dist.config.js
@@ -54,7 +54,7 @@ module.exports = {
     new OptimizeCSSAssetsPlugin(),
     new EndWebpackPlugin(async () => {
       // 自定义域名
-      fs.writeFileSync(path.resolve(outputPath, 'CNAME'), 'miaoshaoxuan.xyz');
+      fs.writeFileSync(path.resolve(outputPath, 'CNAME'), 'www.miaoshaoxuan.xyz');
 
       // 调用 Chrome 渲染出 PDF 文件
       const chromePath = findChrome();
